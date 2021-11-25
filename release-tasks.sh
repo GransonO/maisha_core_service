75b9046e77093b855b2d1140f9e0038aa3de5422
GransonO <oyombegranson@gmail.com>
2021-11-25 13:07:27 +0300

    release shell setup

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set DJANGO_SETTINGS_MODULE=maisha_service.settings
+export DJANGO_SETTINGS_MODULE=maisha_service.settings
+python manage.py shell
 python3 manage.py makemigrations
 python3 manage.py migrate
