f3ccea83bd81f17f1f6c23c90c35a0bcddd67ab0
GransonO <oyombegranson@gmail.com>
2021-11-25 16:15:36 +0300

    daphne

--- release-tasks.sh
+++ release-tasks.sh
@@ -3,4 +3,6 @@ echo ----------- Started release tasks
 export DJANGO_SETTINGS_MODULE=maisha_service.settings
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+python manage.py check
 echo ----------- Completed release tasks
