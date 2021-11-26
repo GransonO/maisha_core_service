f2b459017c82d083c5124d394d844ca83163b732
GransonO <oyombegranson@gmail.com>
2021-11-26 12:57:27 +0300

    Daphne killed me

--- maisha_service/settings.py
+++ maisha_service/settings.py
@@ -159,7 +159,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [
-                os.environ.get('REDIS_URL', 'redis://localhost:6379')
+                os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')
             ],
         },
 
