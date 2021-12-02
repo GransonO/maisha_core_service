f21dc595fc2915ecd60203625624b20ed42f6c68
GransonO <oyombegranson@gmail.com>
2021-12-02 20:34:54 +0300

    type of ?

--- maisha_service/apps/ws_connect/chat_socket.py
+++ maisha_service/apps/ws_connect/chat_socket.py
@@ -31,10 +31,12 @@ class SocketChat(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        print("text_data ----------- {}".format(text_data["message"]))
+        print("text_data ----------- {}".format(type(text_data)))
         chat_data = json.loads(text_data)
+        print("chat_data ----------- {}".format(type(chat_data)))
+        print("chat_data ----------- {}".format(chat_data["message"]))
+
         # Send message to room group
-        print("chat_data ----------- {}".format(text_data))
         chat_result = await self.persist_to_db(chat_data)
         await self.channel_layer.group_send(
             self.room_group_name,
